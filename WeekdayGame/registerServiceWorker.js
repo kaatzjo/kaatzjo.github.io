7f46119587af8386d30dfdfe6d8bd700c7b4cc87
kaatzjo <110397668+kaatzjo@users.noreply.github.com>
2022-09-11 01:46:35 +0200

    added dummy service worker

---
+++ WeekdayGame/registerServiceWorker.js
@@ -0,0 +1,20 @@
+async function registerServiceWorker(){
+    if ("serviceWorker" in navigator){
+        try {
+            const registration = await navigator.serviceWorker.register("/serviceWorker.js");
+            if (registration.installing) {
+                console.log("Service worker installing");
+            } else if (registration.waiting) {
+                console.log("Service worker installed");
+            } else if (registration.active) {
+                console.log("Service worker active");
+            }
+        } catch (error){
+            console.error("`Registration failed with ${error}`");
+        }
+    } else {
+        console.log("service workers not supported");
+    }
+};
+
+registerServiceWorker();
