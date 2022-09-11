2a128dd2a1b500df47436f9b222073b7c6278bbb
kaatzjo <110397668+kaatzjo@users.noreply.github.com>
2022-09-11 20:28:00 +0200

    new service worker can delete old cache, this can be used to force updates

--- WeekdayGame/serviceWorker.js
+++ WeekdayGame/serviceWorker.js
@@ -1,7 +1,35 @@
-CACHE_NAME = 'WeekdayGame-1';
+const CACHE_NAME = 'WeekdayGame-1';
+
+const FORCE_UPDATE = true;
+
+const FILES_TO_CACHE = [
+    'index.html',
+    'settings.html',
+    'stats.html',
+    'more.html',
+    'scripts/index.js',
+    'scripts/settings.js',
+    'scripts/stats.js',
+    'styles/index.css',
+    'styles/main.css',
+    'styles/more.css',
+    'styles/settings.css',
+    'styles/stats.css',
+    'icons/calendar_colorful.png',
+    'icons/calendar.png',
+    'icons/calendar.svg',
+    'icons/info.svg',
+    'icons/QR.svg',
+];
 
 self.addEventListener('install', (event)=>{
     console.log('service worker install event triggered');
+    if (caches.has(CACHE_NAME)){
+        if (!FORCE_UPDATE){
+            return;
+        }
+        caches.delete(CACHE_NAME).then(bool => console.log(bool));
+    }
     fillCache(CACHE_NAME);
 });
 
@@ -11,34 +39,19 @@ self.addEventListener('fetch', (event)=>{
 
 async function fillCache(cacheName){
     const cacheStorage = await caches.open(cacheName);
-    await cacheStorage.addAll([
-        'index.html',
-        'settings.html',
-        'stats.html',
-        'more.html',
-        'scripts/index.js',
-        'scripts/settings.js',
-        'scripts/stats.js',
-        'styles/index.css',
-        'styles/main.css',
-        'styles/more.css',
-        'styles/settings.css',
-        'styles/stats.css',
-        'icons/calendar_colorful.png',
-        'icons/calendar.png',
-        'icons/calendar.svg',
-        'icons/info.svg',
-        'icons/QR.svg',
-    ]);
-};
+    await cacheStorage.addAll(FILES_TO_CACHE);
+}
 
 async function getCachedData(cacheName, url) {
+    if (url === ''){
+        url = 'index.html';
+    }
     const cacheStorage = await caches.open(cacheName);
     const cachedResponse = await cacheStorage.match(url);
 
     if (!cachedResponse || !cachedResponse.ok) {
-        return false;
+        return await fetch(url);
     }
 
     return cachedResponse;
-};
+}
