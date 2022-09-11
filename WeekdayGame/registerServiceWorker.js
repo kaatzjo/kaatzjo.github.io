1ca6b56a0482d47d4d62d3438815d363a289b898
kaatzjo <110397668+kaatzjo@users.noreply.github.com>
2022-09-11 02:01:31 +0200

    small bug fix

--- WeekdayGame/registerServiceWorker.js
+++ WeekdayGame/registerServiceWorker.js
@@ -1,7 +1,7 @@
 async function registerServiceWorker(){
     if ("serviceWorker" in navigator){
         try {
-            const registration = await navigator.serviceWorker.register("/serviceWorker.js");
+            const registration = await navigator.serviceWorker.register("serviceWorker.js");
             if (registration.installing) {
                 console.log("Service worker installing");
             } else if (registration.waiting) {
@@ -10,7 +10,7 @@ async function registerServiceWorker(){
                 console.log("Service worker active");
             }
         } catch (error){
-            console.error("`Registration failed with ${error}`");
+            console.error(`Registration failed with ${error}`);
         }
     } else {
         console.log("service workers not supported");
